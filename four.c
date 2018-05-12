102b2d910cd9712dd0bbb13c7f226dc58c0fa31e
Elias Pol <eli.pol@mail.utoronto.ca>
2018-05-11 23:05:07 -0400

    Simple cases handled

--- four.c
+++ four.c
@@ -13,7 +13,11 @@ int merge(struct eqn a, struct eqn b, struct eqn *o){
 	sprintf(o[1].rep, "(%s) - (%s)", a.rep, b.rep);
 	o[1].eval = a.eval - b.eval;
 	sprintf(o[2].rep, "(%s) / (%s)", a.rep, b.rep);
-	o[2].eval = a.eval / b.eval;
+	if(b.eval != 0) {
+		o[2].eval = a.eval / b.eval;
+	} else {
+		o[2].eval = -99;
+	}
 	sprintf(o[3].rep, "(%s) * (%s)", a.rep, b.rep);
 	o[3].eval = a.eval * b.eval;
 	return 4;
@@ -24,12 +28,24 @@ int main(int argc, char **argv){
 	struct eqn o[4];
 	struct eqn a;
 	struct eqn b;
+	struct eqn c;
+	struct eqn d;
 	a.rep[0] = '4'; a.rep[1] = '\0';
 	b.rep[0] = '4'; b.rep[1] = '\0';
+	c.rep[0] = '4'; b.rep[1] = '\0';
+	d.rep[0] = '4'; b.rep[1] = '\0';
 	a.eval = 4;
 	b.eval = 4;
+	c.eval = 4;
+	d.eval = 4;
 	merge(a, b, o);
-	for(int i=0; i<4; i++){
-		printf("%s = %d\n", o[i].rep, o[i].eval);
+	struct eqn z[4][4][4];
+	for (int i=0; i<4; i++){
+		for (int j=0; j<4; j++){
+			merge(o[i], o[j], z[i][j]);
+			for (int k=0; k<4; k++){
+				printf("%s = %d\n", z[i][j][k].rep, z[i][j][k].eval);
+			}
+		}
 	}
 }
